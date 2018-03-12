f76c72ca6ee13fc20e392dff38ad91b01259b4e3
Luyu Sun <lysun@login-2.deepthought2.umd.edu>
2018-03-12 16:55:25 -0400

    Add maooam/tl_ad_tensor

---
+++ MAOOAM/maooam/tl_ad_tensor.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[24]:
+
+
+import numpy as np
+from params_maooam import ndim, tw, t_run, t_trans, dt, f0
+import aotensor as aotensor_mod
+import integrator
+
+def compute_tltensor(eta):
+    tensor = aotensor_mod.aotensor
+    j_mat = np.zeros([ndim,ndim])
+   
+    eta2 = np.append(eta,1.) 
+    for n in np.arange(0,np.shape(tensor)[0]):
+        i = tensor[n][0]
+        j = tensor[n][1]
+        k = tensor[n][2]
+        v = tensor[n][3]
+    
+        if j!=0:
+            j_mat[i-1,j-1] = j_mat[i-1,j-1] + v*eta2[k-1]
+        
+        if k!=0:
+            j_mat[i-1,k-1] = j_mat[i-1,k-1] + v*eta2[j-1]
+
+    return j_mat
+
+
+
+
+
+
+    
+    
+        
+
